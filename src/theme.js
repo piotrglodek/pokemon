ef80c64f91357c46aadf966805c34c753c51d8dc
Piotr Glodek <pioglodek@gmail.com>
2020-10-02 00:07:25 +0200

    Fix font scaling

--- src/theme.js
+++ src/theme.js
@@ -31,6 +31,10 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing:border-box;
     }
 
+    html{
+      font-size:62.5%;
+    }
+
     body{
         margin:0;
         font-family: 'Roboto', sans-serif;
