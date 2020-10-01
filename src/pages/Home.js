b220655a0cf876b5d22fea115c5926c5c0514cbb
Piotr Glodek <pioglodek@gmail.com>
2020-10-02 00:09:09 +0200

    Add layout for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { withTheme } from 'styled-components';
+import { useSetPageTitle } from '../hooks/useSetPageTitle';
+// components
+import { Container, Typography, Card, Layout } from '../components';
 
-export default function Home() {
+const Home = props => {
+  useSetPageTitle('Home');
+  const {
+    theme: { color },
+  } = props;
+
   return (
     <>
-      <Helmet>
-        <title>Home</title>
-      </Helmet>
-      Home
+      <Container>
+        <Typography.Heading1>What are you looking for?</Typography.Heading1>
+        <Layout.Grid>
+          <Card.Box $bgColor={color.green} to='/pokedex'>
+            <Card.Title>Pokedex</Card.Title>
+          </Card.Box>
+          <Card.Box $bgColor={color.red} to='/moves'>
+            <Card.Title>Moves</Card.Title>
+          </Card.Box>
+          <Card.Box $bgColor={color.blue} to='/abilities'>
+            <Card.Title>Abilities</Card.Title>
+          </Card.Box>
+          <Card.Box $bgColor={color.yellow} to='/items'>
+            <Card.Title>Items</Card.Title>
+          </Card.Box>
+          <Card.Box $bgColor={color.violet} to='/locations'>
+            <Card.Title>Locations</Card.Title>
+          </Card.Box>
+          <Card.Box $bgColor={color.brown} to='/type'>
+            <Card.Title>Type</Card.Title>
+          </Card.Box>
+        </Layout.Grid>
+      </Container>
     </>
   );
-}
+};
+
+export default withTheme(Home);
