9e5167530cd01866efa96463a81e453c9866cecd
Piotr Glodek <pioglodek@gmail.com>
2020-10-01 22:59:51 +0200

    Export Container and Card components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 import * as Typography from './Typography/Typography';
+import * as Card from './Card/Card';
 
 export { Typography };
+export { Card };
+export { Container } from './Container/Container';
