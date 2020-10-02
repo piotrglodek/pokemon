822638e3a33b95e86024ae86a39cd4154e74ee9d
Piotr Glodek <pioglodek@gmail.com>
2020-10-02 13:15:53 +0200

    Fix dynamic rendering routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home } from './pages';
+// components
+import { Container } from './components';
+// pages
+import { Home, Pokedex } from './pages';
+
+const routesList = [
+  { path: '/', Component: Home, pageTitle: 'Home' },
+  { path: '/pokedex', Component: Pokedex, pageTitle: 'Pokedex' },
+];
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-    </Switch>
+    <Container>
+      <Switch>
+        {routesList.map(route => {
+          const { path, Component, pageTitle } = route;
+          return (
+            <Route
+              key={path}
+              exact
+              path={path}
+              component={() => <Component pageTitle={pageTitle} />}
+            />
+          );
+        })}
+      </Switch>
+    </Container>
   );
 }
