18287df1f70bce8d6e4d1bd68d0795162b7b7a66
Piotr Glodek <pioglodek@gmail.com>
2020-10-02 15:18:50 +0200

    Add Pokedex basic structure

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+//hooks
 import { useSetPageTitle } from '../hooks';
+// components
+import { Arrow, Typography, Pokemons } from '../components';
 
 function Pokedex(props) {
   const { pageTitle } = props;
   useSetPageTitle(pageTitle);
+  const handleGoBack = useHistory().goBack;
 
-  return <>Pokedex</>;
+  return (
+    <>
+      <Arrow.Button onClick={handleGoBack}>
+        <Arrow.Back />
+      </Arrow.Button>
+      <Typography.Heading1>{pageTitle}</Typography.Heading1>
+      <Pokemons />
+    </>
+  );
 }
 
 export default Pokedex;
