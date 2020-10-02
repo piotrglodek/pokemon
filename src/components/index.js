2e8b566d0f1493223bc8893a0ea6f4a27cada582
Piotr Glodek <pioglodek@gmail.com>
2020-10-02 13:38:29 +0200

    Add Arrow  component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import * as Typography from './Typography/Typography';
 import * as Card from './Card/Card';
 import * as Layout from './Layout/Layout';
+import * as Arrow from './Arrow/Arrow';
 
 export { Typography };
 export { Card };
 export { Container } from './Container/Container';
 export { Layout };
+export { Arrow };
