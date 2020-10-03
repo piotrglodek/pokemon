bf548930dcc1591f9cd37a41307f9b826ab22ad7
Piotr Glodek <pioglodek@gmail.com>
2020-10-03 18:09:24 +0200

    Add basic pokedex page

--- src/components/Pokemons/Pokemons.js
+++ src/components/Pokemons/Pokemons.js
@@ -1,20 +1,54 @@
 import React from 'react';
 // components
-import { Spinner } from '../';
+import { Spinner, Typography, Card, Layout } from '../';
 // hooks
-import { useFetch } from '../../hooks';
+import { useFetchPokemons } from '../../hooks';
 
 function Pokemons() {
-  const [response, loading] = useFetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=50`
-  );
-
-  if (loading || !response) {
-    return <Spinner />;
+  const [pokemons, loading, errors] = useFetchPokemons();
+  // FIXME: NoData component with image
+  if (errors) {
+    return (
+      <>
+        <Typography.Text>{errors[0]}</Typography.Text>
+        <Typography.Text>
+          {errors[1]}
+          <a target='_blank' rel='noopener noreferrer' href={errors[2]}>
+            github issues
+          </a>
+          .
+        </Typography.Text>
+      </>
+    );
   }
 
-  console.log(response);
-  return <>Data loaded</>;
+  if (loading || !pokemons) {
+    return <Spinner />;
+  } else {
+    const renderPokemonCards = pokemons.map(pokemon => {
+      const { id, name, types } = pokemon;
+      // pokemon types
+      const pokemonTypes = [];
+      types.map(type => pokemonTypes.push(type.type.name));
+      return (
+        <Card.Box $cardColor={pokemonTypes[0]} key={id} to='/'>
+          <Card.Title>{name}</Card.Title>
+          <Card.Wrapper>
+            <Card.BadgesWrapper>
+              {pokemonTypes.map(type => (
+                <Card.Badge key={type}>{type}</Card.Badge>
+              ))}
+            </Card.BadgesWrapper>
+            <Card.Image
+              url={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
+            />
+          </Card.Wrapper>
+        </Card.Box>
+      );
+    });
+
+    return <Layout.Grid>{renderPokemonCards}</Layout.Grid>;
+  }
 }
 
 export default Pokemons;
