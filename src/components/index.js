d21eea3e7d72e82bdc898b4af67beeb30ec293bb
Piotr Glodek <pioglodek@gmail.com>
2020-10-05 17:20:58 +0200

    Fix export components

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,8 @@ import * as Arrow from './Arrow/Arrow';
 
 export { Typography };
 export { Card };
-export { Container } from './Container/Container';
 export { Layout };
 export { Arrow };
 export { Spinner } from './Spinner/Spinner';
-export { default as Pokemons } from './Pokemons/Pokemons';
-export { default as NoData } from './NoData/NoData';
+export { NoData } from './NoData/NoData';
+export { PokemonCard } from './PokemonCard/PokemonCard';
