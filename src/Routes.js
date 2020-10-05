f61eed365b0bd1e5ea060b6d715616bc3f9d14ce
Piotr Glodek <pioglodek@gmail.com>
2020-10-05 17:10:38 +0200

    Change layout and route param
    Added Container to Layout component, now Container must by used
    Layout.Container
    Changed SinglePokemonPage param from id to pokemonName

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // components
-import { Container } from './components';
+import { Layout } from './components';
 // pages
 import { Home, Pokedex, PokemonSinglePage } from './pages';
 
 export default function Routes() {
   return (
-    <Container>
+    <Layout.Container>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/pokedex' component={Pokedex} />
-        <Route exact path='/pokemon/:id' component={PokemonSinglePage} />
+        <Route
+          exact
+          path='/pokemon/:pokemonName'
+          component={PokemonSinglePage}
+        />
       </Switch>
-    </Container>
+    </Layout.Container>
   );
 }
