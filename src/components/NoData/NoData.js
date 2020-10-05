4c412654873ed4505f9b31607fdae4aed0ce14ea
Piotr Glodek <pioglodek@gmail.com>
2020-10-05 13:28:42 +0200

    Add NoData component

---
+++ src/components/NoData/NoData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography } from '../';
+
+function NoData(props) {
+  const { errors } = props;
+  return (
+    <>
+      <Typography.Text>{errors[0]}</Typography.Text>
+      <Typography.Text>
+        {errors[1]}
+        <a target='_blank' rel='noopener noreferrer' href={errors[2]}>
+          github issues
+        </a>
+        .
+      </Typography.Text>
+    </>
+  );
+}
+
+export default NoData;
