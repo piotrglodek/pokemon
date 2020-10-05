f5cbd6c3fafe494fab78740ae6d3d7b667d0dafb
Piotr Glodek <pioglodek@gmail.com>
2020-10-05 14:27:26 +0200

    Remove map rendering Route

--- src/Routes.js
+++ src/Routes.js
@@ -3,28 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 // components
 import { Container } from './components';
 // pages
-import { Home, Pokedex } from './pages';
-
-const routesList = [
-  { path: '/', Component: Home, pageTitle: 'Home' },
-  { path: '/pokedex', Component: Pokedex, pageTitle: 'Pokedex' },
-];
+import { Home, Pokedex, PokemonSinglePage } from './pages';
 
 export default function Routes() {
   return (
     <Container>
       <Switch>
-        {routesList.map(route => {
-          const { path, Component, pageTitle } = route;
-          return (
-            <Route
-              key={path}
-              exact
-              path={path}
-              component={() => <Component pageTitle={pageTitle} />}
-            />
-          );
-        })}
+        <Route exact path='/' component={Home} />
+        <Route exact path='/pokedex' component={Pokedex} />
+        <Route exact path='/pokemon/:id' component={PokemonSinglePage} />
       </Switch>
     </Container>
   );
