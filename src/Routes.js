89304c88fae97700876f6d75782d9b69da22a8eb
Piotr Glodek <pioglodek@gmail.com>
2020-10-06 13:25:24 +0200

    Change PokemonSinglePage component route

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // components
-import { Layout } from './components';
+import { Layout, Pokemon } from './components';
 // pages
-import { Home, Pokedex, PokemonSinglePage } from './pages';
+import { Home, Pokedex } from './pages';
 
 export default function Routes() {
   return (
-    <Layout.Container>
+    <>
+      <Layout.Container>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/pokedex' component={Pokedex} />
+        </Switch>
+      </Layout.Container>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/pokedex' component={Pokedex} />
-        <Route
-          exact
-          path='/pokemon/:pokemonName'
-          component={PokemonSinglePage}
-        />
+        <Route exact path='/pokemon/:pokemonName' component={Pokemon} />
       </Switch>
-    </Layout.Container>
+    </>
   );
 }
